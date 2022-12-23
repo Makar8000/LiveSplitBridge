134d3634050c38f6c01827742f927e9d2e1a75d4
Makar8000 <Makar8000@users.noreply.github.com>
2022-12-23 14:33:39 -0600

    Initial commit

---
+++ BridgeForm.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace LiveSplitBridge
+{
+    partial class BridgeForm
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connectBtn = new System.Windows.Forms.Button();
+            this.disconnectBtn = new System.Windows.Forms.Button();
+            this.logList = new System.Windows.Forms.ListView();
+            this.listColTim = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listColMsg = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.logLabel = new System.Windows.Forms.Label();
+            this.resetModeLbl = new System.Windows.Forms.Label();
+            this.resetModeCmb = new System.Windows.Forms.ComboBox();
+            this.resetHotkeyLbl = new System.Windows.Forms.Label();
+            this.resetHotkeyTxt = new System.Windows.Forms.TextBox();
+            this.resetDefaultBtn = new System.Windows.Forms.Button();
+            this.testSplitBtn = new System.Windows.Forms.Button();
+            this.testResetBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // connectBtn
+            // 
+            this.connectBtn.Location = new System.Drawing.Point(13, 13);
+            this.connectBtn.Name = "connectBtn";
+            this.connectBtn.Size = new System.Drawing.Size(154, 23);
+            this.connectBtn.TabIndex = 0;
+            this.connectBtn.Text = "Connect to LiveSplit";
+            this.connectBtn.UseVisualStyleBackColor = true;
+            this.connectBtn.Click += new System.EventHandler(this.connectBtn_Click);
+            // 
+            // disconnectBtn
+            // 
+            this.disconnectBtn.Enabled = false;
+            this.disconnectBtn.Location = new System.Drawing.Point(174, 13);
+            this.disconnectBtn.Margin = new System.Windows.Forms.Padding(4, 3, 3, 3);
+            this.disconnectBtn.Name = "disconnectBtn";
+            this.disconnectBtn.Size = new System.Drawing.Size(154, 23);
+            this.disconnectBtn.TabIndex = 1;
+            this.disconnectBtn.Text = "Disconnect from LiveSplit";
+            this.disconnectBtn.UseVisualStyleBackColor = true;
+            this.disconnectBtn.Click += new System.EventHandler(this.disconnectBtn_Click);
+            // 
+            // logList
+            // 
+            this.logList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.logList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.listColTim,
+            this.listColMsg});
+            this.logList.FullRowSelect = true;
+            this.logList.HideSelection = false;
+            this.logList.Location = new System.Drawing.Point(14, 119);
+            this.logList.Name = "logList";
+            this.logList.Size = new System.Drawing.Size(504, 180);
+            this.logList.TabIndex = 2;
+            this.logList.UseCompatibleStateImageBehavior = false;
+            this.logList.View = System.Windows.Forms.View.Details;
+            this.logList.KeyUp += new System.Windows.Forms.KeyEventHandler(this.logList_KeyUp);
+            // 
+            // listColTim
+            // 
+            this.listColTim.Text = "Timestamp";
+            this.listColTim.Width = 135;
+            // 
+            // listColMsg
+            // 
+            this.listColMsg.Text = "Message";
+            this.listColMsg.Width = 350;
+            // 
+            // logLabel
+            // 
+            this.logLabel.AutoSize = true;
+            this.logLabel.Location = new System.Drawing.Point(13, 101);
+            this.logLabel.Margin = new System.Windows.Forms.Padding(3, 10, 3, 2);
+            this.logLabel.Name = "logLabel";
+            this.logLabel.Size = new System.Drawing.Size(60, 13);
+            this.logLabel.TabIndex = 3;
+            this.logLabel.Text = "Debug Log";
+            // 
+            // resetModeLbl
+            // 
+            this.resetModeLbl.AutoSize = true;
+            this.resetModeLbl.Location = new System.Drawing.Point(13, 49);
+            this.resetModeLbl.Margin = new System.Windows.Forms.Padding(3, 10, 3, 2);
+            this.resetModeLbl.Name = "resetModeLbl";
+            this.resetModeLbl.Size = new System.Drawing.Size(65, 13);
+            this.resetModeLbl.TabIndex = 4;
+            this.resetModeLbl.Text = "Reset Mode";
+            // 
+            // resetModeCmb
+            // 
+            this.resetModeCmb.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.resetModeCmb.FormattingEnabled = true;
+            this.resetModeCmb.Items.AddRange(new object[] {
+            "Hotkey",
+            "Server"});
+            this.resetModeCmb.Location = new System.Drawing.Point(16, 67);
+            this.resetModeCmb.Name = "resetModeCmb";
+            this.resetModeCmb.Size = new System.Drawing.Size(100, 21);
+            this.resetModeCmb.TabIndex = 5;
+            this.resetModeCmb.SelectedIndex = 0;
+            // 
+            // resetHotkeyLbl
+            // 
+            this.resetHotkeyLbl.AutoSize = true;
+            this.resetHotkeyLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetHotkeyLbl.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.resetHotkeyLbl.Location = new System.Drawing.Point(122, 49);
+            this.resetHotkeyLbl.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
+            this.resetHotkeyLbl.Name = "resetHotkeyLbl";
+            this.resetHotkeyLbl.Size = new System.Drawing.Size(72, 13);
+            this.resetHotkeyLbl.TabIndex = 6;
+            this.resetHotkeyLbl.Text = "Reset Hotkey";
+            this.resetHotkeyLbl.Click += new System.EventHandler(this.resetHotkeyLbl_Click);
+            // 
+            // resetHotkeyTxt
+            // 
+            this.resetHotkeyTxt.Location = new System.Drawing.Point(122, 67);
+            this.resetHotkeyTxt.Name = "resetHotkeyTxt";
+            this.resetHotkeyTxt.Size = new System.Drawing.Size(100, 20);
+            this.resetHotkeyTxt.TabIndex = 7;
+            this.resetHotkeyTxt.Text = "{F15}";
+            // 
+            // resetDefaultBtn
+            // 
+            this.resetDefaultBtn.Location = new System.Drawing.Point(228, 65);
+            this.resetDefaultBtn.Name = "resetDefaultBtn";
+            this.resetDefaultBtn.Size = new System.Drawing.Size(100, 23);
+            this.resetDefaultBtn.TabIndex = 8;
+            this.resetDefaultBtn.Text = "Reset to Default";
+            this.resetDefaultBtn.UseVisualStyleBackColor = true;
+            this.resetDefaultBtn.Click += new System.EventHandler(this.resetDefaultBtn_Click);
+            // 
+            // testSplitBtn
+            // 
+            this.testSplitBtn.Enabled = false;
+            this.testSplitBtn.Location = new System.Drawing.Point(358, 25);
+            this.testSplitBtn.Name = "testSplitBtn";
+            this.testSplitBtn.Size = new System.Drawing.Size(75, 23);
+            this.testSplitBtn.TabIndex = 9;
+            this.testSplitBtn.Text = "Test Split";
+            this.testSplitBtn.UseVisualStyleBackColor = true;
+            this.testSplitBtn.Click += new System.EventHandler(this.testSplitBtn_Click);
+            // 
+            // testResetBtn
+            // 
+            this.testResetBtn.Enabled = false;
+            this.testResetBtn.Location = new System.Drawing.Point(358, 54);
+            this.testResetBtn.Name = "testResetBtn";
+            this.testResetBtn.Size = new System.Drawing.Size(75, 23);
+            this.testResetBtn.TabIndex = 10;
+            this.testResetBtn.Text = "Test Reset";
+            this.testResetBtn.UseVisualStyleBackColor = true;
+            this.testResetBtn.Click += new System.EventHandler(this.testResetBtn_Click);
+            // 
+            // BridgeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.testResetBtn);
+            this.Controls.Add(this.testSplitBtn);
+            this.Controls.Add(this.resetDefaultBtn);
+            this.Controls.Add(this.resetHotkeyTxt);
+            this.Controls.Add(this.resetHotkeyLbl);
+            this.Controls.Add(this.resetModeCmb);
+            this.Controls.Add(this.resetModeLbl);
+            this.Controls.Add(this.logLabel);
+            this.Controls.Add(this.logList);
+            this.Controls.Add(this.disconnectBtn);
+            this.Controls.Add(this.connectBtn);
+            this.Name = "BridgeForm";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.Size = new System.Drawing.Size(531, 312);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connectBtn;
+        private System.Windows.Forms.Button disconnectBtn;
+        private System.Windows.Forms.ListView logList;
+        private System.Windows.Forms.ColumnHeader listColTim;
+        private System.Windows.Forms.ColumnHeader listColMsg;
+        private System.Windows.Forms.Label logLabel;
+        private System.Windows.Forms.Label resetModeLbl;
+        private System.Windows.Forms.ComboBox resetModeCmb;
+        private System.Windows.Forms.Label resetHotkeyLbl;
+        private System.Windows.Forms.TextBox resetHotkeyTxt;
+        private System.Windows.Forms.Button resetDefaultBtn;
+        private System.Windows.Forms.Button testSplitBtn;
+        private System.Windows.Forms.Button testResetBtn;
+    }
+}
---
+++ BridgeForm.cs
@@ -0,0 +1,237 @@
+﻿using Advanced_Combat_Tracker;
+using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace LiveSplitBridge {
+  public partial class BridgeForm:UserControl, IActPluginV1 {
+    private Label pluginLabel;
+    private string settingsFile;
+    private SettingsSerializer xmlSettings;
+    private NamedPipeClientStream client;
+    private StreamWriter clientWriter;
+    private readonly string SENDKEYS_HELP_LINK = "https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.sendkeys.send?view=netframework-4.8#remarks";
+    public delegate void CustomCallbackDelegate(object o, string param);
+
+    public BridgeForm() {
+      InitializeComponent();
+      InitTrigg();
+    }
+
+    #region IActPluginV1 Members
+    public void InitPlugin(TabPage pluginScreenSpace, Label pluginStatusText) {
+      // ACT Stuff
+      pluginLabel = pluginStatusText;
+      pluginScreenSpace.Controls.Add(this);
+      pluginScreenSpace.Text = "LiveSplit Bridge";
+      Dock = DockStyle.Fill;
+
+      // Settings
+      string configName = "LiveSplitBridge";
+      try {
+        string pluginName = ActGlobals.oFormActMain.PluginGetSelfData(this).pluginFile.FullName;
+        pluginName = Path.GetFileNameWithoutExtension(pluginName).Trim();
+        if (pluginName.Length >= 0)
+          configName = pluginName;
+      } catch (Exception) { }
+      settingsFile = Path.Combine(ActGlobals.oFormActMain.AppDataFolder.FullName, $"Config\\{configName}.config.xml");
+      xmlSettings = new SettingsSerializer(this);
+      LoadSettings();
+
+      // Finish and update status
+      pluginLabel.Text = "Plugin Started";
+      Log($"Plugin loaded.");
+    }
+
+    public void DeInitPlugin() {
+      SaveSettings();
+      pluginLabel.Text = "Plugin Exited";
+    }
+    #endregion
+
+    #region LiveSplit Functions
+    public void WriteToPipe(string text) {
+      try {
+        if (text != "reset" || resetModeCmb.Text == "Server") {
+          clientWriter.WriteLine(text);
+          Log($"Wrote to pipe: {text}");
+        } else {
+          SendKeys.Send(resetHotkeyTxt.Text);
+          Log($"Reset hotkey fired: {resetHotkeyTxt.Text}");
+        }
+      } catch (Exception ex) {
+        Log($"[Error][{text}] {ex}");
+      }
+    }
+
+    private void Connect() {
+      try {
+        connectBtn.Enabled = false;
+        Log($"Connecting...");
+
+        client = new NamedPipeClientStream(".", "LiveSplit", PipeDirection.Out, PipeOptions.Asynchronous);
+        client.Connect(3000);
+        clientWriter = new StreamWriter(client);
+        clientWriter.AutoFlush = true;
+
+        disconnectBtn.Enabled = true;
+        testSplitBtn.Enabled = true;
+        testResetBtn.Enabled = true;
+
+        Log($"Connected to LiveSplit named pipe.");
+      } catch (TimeoutException) {
+        connectBtn.Enabled = true;
+        Log($"Connection timed out. Please make sure LiveSplit is open.");
+      } catch (Exception ex) {
+        connectBtn.Enabled = true;
+        Log($"[Error Connecting] {ex}");
+      }
+    }
+
+    private void Disconnect() {
+      try {
+        disconnectBtn.Enabled = false;
+        Log($"Disconnecting...");
+
+        clientWriter.Dispose();
+        client.Dispose();
+
+        connectBtn.Enabled = true;
+        disconnectBtn.Enabled = false;
+        testSplitBtn.Enabled = false;
+        testResetBtn.Enabled = false;
+
+        Log($"Disconnected.");
+      } catch (Exception ex) {
+        disconnectBtn.Enabled = true;
+        Log($"[Error Disconnecting] {ex}");
+      }
+    }
+    #endregion
+
+    #region Triggernometry Integration
+    private void InitTrigg() {
+      try {
+        var trigg = ActGlobals.oFormActMain.ActPlugins.FirstOrDefault(x => x.lblPluginTitle.Text == "Triggernometry.dll");
+        if (trigg == null || trigg.pluginObj == null)
+          return;
+
+        var triggType = trigg.pluginObj.GetType();
+        var deleType = triggType.GetNestedType("CustomCallbackDelegate");
+        if (deleType == null)
+          return;
+
+        var dele = Delegate.CreateDelegate(deleType, this, typeof(BridgeForm).GetMethod("HandleTriggMessage"));
+        triggType.GetMethod("RegisterNamedCallback")?.Invoke(trigg.pluginObj, new object[] { "LiveSplitMessage", dele, null });
+        Log($"Registered Triggernometry callback \"LiveSplitMessage\".");
+      } catch (Exception ex) {
+        Log($"Failed to register Triggernometry callback: {ex}");
+      }
+    }
+
+    public void HandleTriggMessage(object _, string msg) {
+      Log($"Received message from Trigg: {msg}");
+      if (client != null && client.IsConnected)
+        WriteToPipe(msg);
+    }
+    #endregion
+
+    #region UI Events
+    private void resetDefaultBtn_Click(object sender, EventArgs e) {
+      resetModeCmb.SelectedIndex = 0;
+      resetHotkeyTxt.Text = "{F15}";
+    }
+
+    private void logList_KeyUp(object sender, KeyEventArgs e) {
+      if (sender != logList)
+        return;
+
+      if (e.Control && e.KeyCode == Keys.C && logList.SelectedItems.Count > 0) {
+        var builder = new StringBuilder();
+        foreach (ListViewItem item in logList.SelectedItems)
+          builder.AppendLine(item.SubItems[1].Text);
+
+        string clipboard = builder.ToString();
+        if (clipboard.Length > 0)
+          Clipboard.SetText(builder.ToString());
+      }
+    }
+
+    private async void connectBtn_Click(object sender, EventArgs e) {
+      await Task.Run(Connect);
+    }
+
+    private async void disconnectBtn_Click(object sender, EventArgs e) {
+      await Task.Run(Disconnect);
+    }
+
+    private void testSplitBtn_Click(object sender, EventArgs e) {
+      WriteToPipe("startorsplit");
+    }
+
+    private void testResetBtn_Click(object sender, EventArgs e) {
+      WriteToPipe("reset");
+    }
+
+    private void resetHotkeyLbl_Click(object sender, EventArgs e) {
+      System.Diagnostics.Process.Start(SENDKEYS_HELP_LINK);
+    }
+    #endregion
+
+    #region Settings
+    public void Log(string text) {
+      string[] row = new string[2];
+      row[0] = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToLongTimeString();
+      row[1] = text;
+      logList.Items.Add(new ListViewItem(row));
+      logList.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+    }
+
+    public void LoadSettings() {
+      xmlSettings.AddControlSetting(resetHotkeyTxt.Name, resetHotkeyTxt);
+      xmlSettings.AddControlSetting(resetModeCmb.Name, resetModeCmb);
+      if (File.Exists(settingsFile)) {
+        FileStream fs = new FileStream(settingsFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+        XmlTextReader xReader = new XmlTextReader(fs);
+        try {
+          while (xReader.Read())
+            if (xReader.NodeType == XmlNodeType.Element)
+              if (xReader.LocalName == "SettingsSerializer")
+                xmlSettings.ImportFromXml(xReader);
+        } catch (Exception ex) {
+          pluginLabel.Text = "Error loading settings: " + ex.Message;
+        }
+        xReader.Close();
+      }
+      resetModeCmb.DropDownStyle = ComboBoxStyle.DropDownList;
+    }
+
+    public bool SaveSettings() {
+      try {
+        FileStream fs = new FileStream(settingsFile, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+        XmlTextWriter xWriter = new XmlTextWriter(fs, Encoding.UTF8);
+        xWriter.Formatting = Formatting.Indented;
+        xWriter.Indentation = 1;
+        xWriter.IndentChar = '\t';
+        xWriter.WriteStartDocument(true);
+        xWriter.WriteStartElement("Config");
+        xWriter.WriteStartElement("SettingsSerializer");
+        xmlSettings.ExportToXml(xWriter);
+        xWriter.WriteEndElement();
+        xWriter.WriteEndElement();
+        xWriter.WriteEndDocument();
+        xWriter.Flush();
+        xWriter.Close();
+      } catch {
+        return false;
+      }
+      return true;
+    }
+    #endregion
+  }
+}
